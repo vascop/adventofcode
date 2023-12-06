94496e18de6d27591d35a40482e34151c09af777
Vasco Pinho <vasco.pinho@hotjar.com>
2023-12-06 01:57:04 +0100

    2023 day 2

---
+++ 2023/2.py
@@ -0,0 +1,34 @@
+# part 1
+import re
+
+goals = {'red': 12, 'green': 13, 'blue': 14}
+sumis = 0
+
+for gameno, line in enumerate(open('2.input.txt')):
+	failed = 0
+	for draw in line.split(':')[1].split(';'):
+		for picked in draw.split(','):
+			number, color = re.findall(r'\w+', picked)
+			if goals[color] < int(number): # too many
+				failed = 1
+				break
+	if failed == 0: 
+		sumis += gameno+1
+print(sumis)
+
+
+# part 2
+import math
+
+sumis = 0
+
+for gameno, line in enumerate(open('2.input.txt')):
+	maxseen = {}
+	power = 0
+	for draw in line.split(':')[1].split(';'):
+		for picked in draw.split(','):
+			number, color = re.findall(r'\w+', picked)
+			if int(number) > maxseen.get(color, 0):
+				maxseen[color] = int(number)
+	sumis += math.prod(maxseen.values())
+print(sumis)
